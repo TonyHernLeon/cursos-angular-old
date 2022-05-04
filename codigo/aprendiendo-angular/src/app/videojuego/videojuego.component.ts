a1215a37846a8205c8d2c1516ff61bd62421afaa
Antonio Hernández León <antoniohl@MacBook-Pro-15-Antonio.local>
2022-05-04 22:37:29 +0200

    Se añaden propiedades (atributos) a videojuego.component.ts

--- codigo/aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ codigo/aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -2,19 +2,15 @@ import { Component } from "@angular/core";
 
 @Component ({
     selector: 'videojuego',
-    template: `
-            <h2>Componente de videjuego</h2>
-            <ul>
-                <li>GTA</li>
-                <li>Prince of Persia</li>
-                <li>Tekken</li>
-                <li>Mario</li>
-            </ul>
-        `
+    templateUrl: './videojuego.component.html'
 })
 export class VideojuegoComponent{
-    
+    public titulo: string;
+    public textoListado: string;
     constructor(){
+
+        this.titulo = "Componente de videjuego";
+        this.textoListado="Listado de juegos";
         console.log("Se ha cargado el componente: VideojuegoComponent");
     }
 }
