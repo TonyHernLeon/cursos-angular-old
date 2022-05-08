744f79605297848703c4d88028773b908f7160f7
Antonio Hernández León <antoniohl91@gmail.com>
2022-05-08 11:18:24 +0200

    Metemos hooks en cursos

--- codigo/aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ codigo/aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, DoCheck, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log("Se inicia el objeto!!!");
+  }
+
+  ngDoCheck(): void { 
+    console.log("Se ha modificado el objecto!!");
+  }
+
+  ngOnDestroy(): void {
+      console.log("Se ha destruido el objecto");
   }
 
 }
