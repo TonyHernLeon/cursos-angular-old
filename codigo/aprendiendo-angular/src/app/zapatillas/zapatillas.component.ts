fe0078dc3a516ea4d6ef534a73fdc22f1aed4c5c
Antonio Hernández León <antoniohl91@gmail.com>
2022-05-08 16:39:20 +0200

    zapatillas.ts: Se actualiza la clase zapatillas.component.ts: Añadimos hook OnInit y cargamos array app.component.html: Comentamos lo que no necesitamos

--- codigo/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ codigo/aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Zapatilla } from '../models/zapatillas'; 
 @Component({
     selector: 'zapatillas',
     templateUrl: './zapatillas.component.html'
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de zapatillas";
+    public zapatillas: Array<Zapatillas>;
 
+    constructor(){
+        this.zapatillas =[
+            new Zapatilla("Reebook Classic", 40, "Blanco", "Reebok", true),
+            new Zapatilla("Nike Runner", 65, "Nike", "Negras",true),
+            new Zapatilla("Adidas SuperStar", 100, "Adidas", "Blanco",false),
+            new Zapatilla("Fila Ladrillo",55,"Fila","Blanco",false)
+        ];
+    }
+
+    ngOnInit(): void {
+        console.log(this.zapatillas);
+    }
 }
