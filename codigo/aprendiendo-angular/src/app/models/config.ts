0a4b2f6de39322bcdc7eb3d070fe9753cd8bed72
Antonio Hernández <antoniohl91@gmail.com>
2022-05-08 10:58:41 +0200

    Creamos clase models.ts para configuracion

---
+++ codigo/aprendiendo-angular/src/app/models/config.ts
@@ -0,0 +1,8 @@
+import { bindCallback } from "rxjs";
+
+export var config = {
+    colorText: "#000000",
+    colorBack: "#eee",
+    tituloPrincipal: "Curso de Angular",
+    descripcionPrincipal: "Aprendiendo Angular cualquier día de la semana"
+}
